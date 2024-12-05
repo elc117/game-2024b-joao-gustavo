091e21af9de8bf858f78dcdb8c78d17333bc8302
jalobler7 <jablobler@inf.ufsm.br>
2024-12-05 14:50:56 -0300

    Organizando inimigos

--- core/src/main/java/com/JogoGustavoEJoao/jogoteste/Main.java
+++ core/src/main/java/com/JogoGustavoEJoao/jogoteste/Main.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+
 import java.util.Iterator;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -56,6 +57,7 @@ public class Main extends ApplicationAdapter {
 
         bitmap = new BitmapFont();
         bitmap.setColor(Color.WHITE);
+        spawnEnemies();
 
         gameover = false;
     }
@@ -64,7 +66,7 @@ public class Main extends ApplicationAdapter {
     public void render() {
         this.moveNave();
         this.moveMissile();
-        this.moveEnemies();
+        //this.moveEnemies();
 
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
@@ -122,78 +124,121 @@ public class Main extends ApplicationAdapter {
                 posX -= velocity;
             }
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            if (posY < Gdx.graphics.getHeight() - nave.getHeight()) {
-                posY += velocity;
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            if (posY > 0) {
-                posY -= velocity;
-            }
-        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+//            if (posY < Gdx.graphics.getHeight() - nave.getHeight()) {
+//                posY += velocity;
+//            }
+//        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+//            if (posY > 0) {
+//                posY -= velocity;
+//            }
+//        }
     }
 
+    //    private void moveMissile() {
+//        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
+//            attack = true;
+//            yMissile = posY + nave.getHeight() / 2 - 12;
+//        }
+//
+//        if (attack) {
+//            if (xMissile < Gdx.graphics.getWidth()) {
+//                xMissile += 40;
+//            } else {
+//                xMissile = posX + nave.getWidth() / 2;
+//                attack = false;
+//            }
+//        } else {
+//            xMissile = posX + nave.getWidth() / 2;
+//            yMissile = posY + nave.getHeight() / 2 - 12;
+//        }
+//    }
     private void moveMissile() {
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
             attack = true;
-            yMissile = posY + nave.getHeight() / 2 - 12;
+            xMissile = posX + nave.getHeight() / 2 - 12;
         }
 
         if (attack) {
-            if (xMissile < Gdx.graphics.getWidth()) {
-                xMissile += 40;
+            if (yMissile < Gdx.graphics.getWidth()) {
+                yMissile += 40;
             } else {
-                xMissile = posX + nave.getWidth() / 2;
+                yMissile = posY + nave.getWidth() / 2;
                 attack = false;
             }
         } else {
-            xMissile = posX + nave.getWidth() / 2;
-            yMissile = posY + nave.getHeight() / 2 - 12;
+            yMissile = posY + nave.getWidth() / 2;
+            xMissile = posX + nave.getHeight() / 2 - 12;
         }
     }
 
+    //    private void spawnEnemies() {
+//        Rectangle enemy = new Rectangle(
+//            Gdx.graphics.getWidth(),
+//            MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight()),
+//            tEnemy.getWidth(),
+//            tEnemy.getHeight()
+//        );
+//        enemies.add(enemy);
+//        lastEnemyTime = TimeUtils.nanoTime();
+//    }
+//
+//    private void moveEnemies() {
+//        if (TimeUtils.nanoTime() - lastEnemyTime > numEnemies) {
+//            this.spawnEnemies();
+//        }
+//
+//        for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext();) {
+//            Rectangle enemy = iter.next();
+//            enemy.x -= 400 * Gdx.graphics.getDeltaTime();
+//
+//            // Colisão com o míssil
+//            if (collide(enemy.x, enemy.y, enemy.width, enemy.height, xMissile, yMissile, missile.getWidth(), missile.getHeight()) && attack) {
+//                ++score;
+//                if (score % 10 == 0) {
+//                    numEnemies -= 100;
+//                }
+//                attack = false;
+//                iter.remove();
+//            } else if (collide(enemy.x, enemy.y, enemy.width, enemy.height, posX, posY, nave.getWidth(), nave.getHeight()) && !gameover) {
+//                --power;
+//                if (power <= 0) {
+//                    gameover = true;
+//                }
+//                iter.remove();
+//            }
+//
+//            if (enemy.x + tEnemy.getWidth() < 0) {
+//                iter.remove();
+//            }
+//        }
+//    }
     private void spawnEnemies() {
-        Rectangle enemy = new Rectangle(
-            Gdx.graphics.getWidth(),
-            MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight()),
-            tEnemy.getWidth(),
-            tEnemy.getHeight()
-        );
-        enemies.add(enemy);
-        lastEnemyTime = TimeUtils.nanoTime();
-    }
+        // Limpa a lista de inimigos caso já exista
+        enemies.clear();
 
-    private void moveEnemies() {
-        if (TimeUtils.nanoTime() - lastEnemyTime > numEnemies) {
-            this.spawnEnemies();
-        }
+        // Calcula a largura disponível entre os inimigos
+        float enemySpacing = Gdx.graphics.getWidth() / 4.0f;
 
-        for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext();) {
-            Rectangle enemy = iter.next();
-            enemy.x -= 400 * Gdx.graphics.getDeltaTime();
+        // Cria 3 inimigos estáticos no topo da tela
+        for (int i = 0; i < 3; i++) {
+            float enemyX = enemySpacing * (i + 1) - tEnemy.getWidth() / 2; // Posiciona os inimigos
+            float enemyY = Gdx.graphics.getHeight() - tEnemy.getHeight(); // No topo da tela
 
-            // Colisão com o míssil
-            if (collide(enemy.x, enemy.y, enemy.width, enemy.height, xMissile, yMissile, missile.getWidth(), missile.getHeight()) && attack) {
-                ++score;
-                if (score % 10 == 0) {
-                    numEnemies -= 100;
-                }
-                attack = false;
-                iter.remove();
-            } else if (collide(enemy.x, enemy.y, enemy.width, enemy.height, posX, posY, nave.getWidth(), nave.getHeight()) && !gameover) {
-                --power;
-                if (power <= 0) {
-                    gameover = true;
-                }
-                iter.remove();
-            }
+            // Cria um novo retângulo para o inimigo
+            Rectangle enemy = new Rectangle(
+                enemyX,
+                enemyY,
+                tEnemy.getWidth(),
+                tEnemy.getHeight()
+            );
 
-            if (enemy.x + tEnemy.getWidth() < 0) {
-                iter.remove();
-            }
+            // Adiciona o inimigo à lista de inimigos
+            enemies.add(enemy);
         }
     }
+
 
     private boolean collide(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
         return x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2;
