4ad8614bfac18fca177cf5100263cb16028d1357
jalobler7 <jablobler@inf.ufsm.br>
2024-12-05 20:15:14 -0300

    Perguntas e respostas

--- core/src/main/java/com/JogoGustavoEJoao/jogoteste/Main.java
+++ core/src/main/java/com/JogoGustavoEJoao/jogoteste/Main.java
@@ -37,11 +37,9 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         Perguntas = new Array<>();
-        Perguntas.add(new Pergunta(
-            "Qual é a cor do céu?",
-            new String[]{"Azul", "Verde", "Amarelo"},
-            0 // Resposta correta: Azul
-        ));
+        Perguntas.add(new Pergunta("Qual é a cor do céu?", new String[]{"Azul", "Verde", "Amarelo"}, 0)
+        );
+        currentQuestion = Perguntas.get(0);
 
 
         batch = new SpriteBatch();
@@ -69,9 +67,9 @@ public class Main extends ApplicationAdapter {
 
         bitmap = new BitmapFont();
         bitmap.setColor(Color.WHITE);
-        //while(!gameover) {
+
         spawnEnemies();
-        // }
+
 
         gameover = false;
     }
@@ -80,10 +78,7 @@ public class Main extends ApplicationAdapter {
     public void render() {
         this.moveNave();
         this.moveMissile();
-        //this.moveEnemies();
         this.checkCollisions();
-
-        //this.spawnEnemies();
 
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
@@ -95,20 +90,21 @@ public class Main extends ApplicationAdapter {
             }
             batch.draw(nave, posX, posY);
 
-            for (Rectangle enemy : enemies) {
-                batch.draw(tEnemy, enemy.x, enemy.y);
+            // Desenha inimigos com respostas
+            for (int i = 0; i < enemies.size; i++) {
+                Rectangle enemy = enemies.get(i);
+                batch.draw(tEnemy, enemy.x, enemy.y, enemy.width, enemy.height);
+                bitmap.draw(batch, currentQuestion.answers[i], enemy.x + 80, enemy.y + enemy.height / 2);
             }
 
-            bitmap.draw(batch, "Pergunta: Pergunta de exemplo", 20, Gdx.graphics.getHeight() - 620);
+            // Exibe a pergunta na parte inferior
+            bitmap.draw(batch, "Pergunta: " + currentQuestion.text, 20, 150);
             bitmap.draw(batch, "Score: " + score, 20, Gdx.graphics.getHeight() - 20);
             bitmap.draw(batch, "Power: " + power, Gdx.graphics.getWidth() - 150, Gdx.graphics.getHeight() - 20);
         } else {
-
-            bitmap.draw(batch, "Score: " + score, 20, Gdx.graphics.getHeight() - 20);
             bitmap.draw(batch, "GAME OVER", Gdx.graphics.getWidth() / 2 - 50, Gdx.graphics.getHeight() / 2);
 
             if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
-
                 score = 0;
                 power = 3;
                 posX = 0;
@@ -119,7 +115,6 @@ public class Main extends ApplicationAdapter {
 
         batch.end();
     }
-
     @Override
     public void dispose() {
         batch.dispose();
@@ -143,24 +138,7 @@ public class Main extends ApplicationAdapter {
         }
     }
 
-    //    private void moveMissile() {
-//        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
-//            attack = true;
-//            yMissile = posY + nave.getHeight() / 2 - 12;
-//        }
-//
-//        if (attack) {
-//            if (xMissile < Gdx.graphics.getWidth()) {
-//                xMissile += 40;
-//            } else {
-//                xMissile = posX + nave.getWidth() / 2;
-//                attack = false;
-//            }
-//        } else {
-//            xMissile = posX + nave.getWidth() / 2;
-//            yMissile = posY + nave.getHeight() / 2 - 12;
-//        }
-//    }
+
 
     private void checkCollisions() {
         for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ) {
@@ -202,73 +180,22 @@ public class Main extends ApplicationAdapter {
         }
     }
 
-    //    private void spawnEnemies() {
-//        Rectangle enemy = new Rectangle(
-//            Gdx.graphics.getWidth(),
-//            MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight()),
-//            tEnemy.getWidth(),
-//            tEnemy.getHeight()
-//        );
-//        enemies.add(enemy);
-//        lastEnemyTime = TimeUtils.nanoTime();
-//    }
-//
-//    private void moveEnemies() {
-//        if (TimeUtils.nanoTime() - lastEnemyTime > numEnemies) {
-//            this.spawnEnemies();
-//        }
-//
-//        for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext();) {
-//            Rectangle enemy = iter.next();
-//            enemy.x -= 400 * Gdx.graphics.getDeltaTime();
-//
-//            // Colisão com o míssil
-//            if (collide(enemy.x, enemy.y, enemy.width, enemy.height, xMissile, yMissile, missile.getWidth(), missile.getHeight()) && attack) {
-//                ++score;
-//                if (score % 10 == 0) {
-//                    numEnemies -= 100;
-//                }
-//                attack = false;
-//                iter.remove();
-//            } else if (collide(enemy.x, enemy.y, enemy.width, enemy.height, posX, posY, nave.getWidth(), nave.getHeight()) && !gameover) {
-//                --power;
-//                if (power <= 0) {
-//                    gameover = true;
-//                }
-//                iter.remove();
-//            }
-//
-//            if (enemy.x + tEnemy.getWidth() < 0) {
-//                iter.remove();
-//            }
-//        }
-//    }
-    private void spawnEnemies() {
 
+    private void spawnEnemies() {
         enemies.clear();
 
+        float enemySpacing = Gdx.graphics.getWidth() / 4.0f;
+        float enemyWidth = tEnemy.getWidth() * 1.5f;
+        float enemyHeight = tEnemy.getHeight() * 1.5f;
 
-        float enemySpacing = Gdx.graphics.getWidth() / 4;
+        for (int i = 0; i < currentQuestion.answers.length; i++) {
+            float enemyX = enemySpacing * (i + 1) - enemyWidth / 2;
+            float enemyY = Gdx.graphics.getHeight() - enemyHeight;
 
-
-
-
-        for (int i = 0; i < 3; i++) {
-            float enemyX = enemySpacing * (i + 1) - tEnemy.getWidth()/2; // Posiciona os inimigos
-            float enemyY = Gdx.graphics.getHeight() - tEnemy.getHeight(); // No topo da tela
-
-
-            Rectangle enemy = new Rectangle(
-                enemyX,
-                enemyY,
-                tEnemy.getWidth(),
-                tEnemy.getHeight()
-            );
-
-
+            Rectangle enemy = new Rectangle(enemyX, enemyY, enemyWidth, enemyHeight);
             enemies.add(enemy);
+        }
     }
-}
 
     private boolean collide(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
         return x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2;
